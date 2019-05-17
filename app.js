809d2d21804ab80d615555141775681a1218cec3
manelcharouni <manelcharouni@think-it.io>
2019-05-17 16:21:50 +0100

    added the Event mongodb model and the CRUD operators to create and fetch events

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
 
+const Event = require('./models/event')
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -42,25 +41,43 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+                .then(events => {
+                    console.log(events); return events.map(event => { return { ...event._doc, _id: event._doc._id.toString() } })
+                })
+                .catch(err => {
+                    throw err
+                })
 
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: + args.eventInput.price,
+            //     date: args.eventInput.date
+            // }
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: + args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event)
-            return event;
+                date: new Date(args.eventInput.date)
+            })
+            return event.save().then(
+                result => {
+                    console.log(result)
+                    return { ...result._doc, _id: result._doc._id.toString() };
+                }
+
+            )
+                .catch(err => { throw err });
         }
 
     },
     graphiql: true
 }))
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@manel-cluster-8tc3w.mongodb.net/test?retryWrites=true`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@manel-cluster-8tc3w.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
     .then(() =>
         app.listen(3000)
     ).catch(err => console.log(err))
