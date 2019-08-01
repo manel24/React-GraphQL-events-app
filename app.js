316792ccf62aae15700a6b62ebe3eda1ba54663b
manelcharouni <manelcharouni@think-it.io>
2019-08-01 14:40:32 +0100

    replacing then catch chaining with async awwait and added some code modularity

--- app.js
+++ app.js
@@ -1,115 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
-const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-
-const Event = require('./models/event')
-const User = require('./models/user')
+const graphQlSchema = require('./graphql/schema')
+const graphQlresolvers = require('./graphql/resolvers')
 const app = express();
 
 app.use(bodyParser.json());
 
+
 app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String
-    }
-    
-    type User{
-        email: String!
-        password: String
-    }
-    input EventInput {
-        title: String!
-        description: String! 
-        price: Float!
-        date: String!
-    }
-
-    input UserInput {
-        email: String!
-        password: String!
-    }
-    type RootQuery{
-        events: [Event!]!
-    }
-
-    type RootMutation{
-        createEvent( eventInput: EventInput): Event
-        createUser( userInput: UserInput): User
-    }
-    schema{
-        query: RootQuery
-        mutation: RootMutation
-    }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-                .then(events => {
-                    console.log(events); return events.map(event => { return { ...event._doc, _id: event.id } })
-                })
-                .catch(err => {
-                    throw err
-                })
-
-        },
-        createEvent: (args) => {
-            let createdEvent;
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: + args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: '5d429e1411731877626b90e3'
-            })
-            return event
-                .save().then(
-                    result => {
-                        createdEvent = { ...result._doc, _id: result._doc._id.toString() }
-                        return User.findById('5d429e1411731877626b90e3')
-                            .then(user => {
-                                if (!user)
-                                    throw (new Error('User not found!'))
-                                user.createdEvents.push(event)
-                                return user.save()
-                            })
-                            .then(result => { return createdEvent })
-                    }
-
-                )
-                .catch(err => { throw err });
-        },
-        createUser: (args) => {
-            return User.findOne({ email: args.userInput.email })
-                .then((user) => {
-                    if (user) {
-                        throw new Error("user already exits!")
-                    }
-                    return bcrypt.hash(args.userInput.password, 12).then(
-                        (hashedPasswd) => {
-                            const user = new User({ email: args.userInput.email, password: hashedPasswd })
-                            return user.save()
-                        }).then(
-                            (result) => {
-                                console.log(result)
-                                return { ...result._doc, _id: result.id, password: null };
-                            }
-
-                        )
-                })
-
-                .catch(err => { throw err });
-        }
-
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlresolvers,
     graphiql: true
 }))
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@manel-cluster-8tc3w.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
