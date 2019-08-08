a20c15c76ac10cf3453d3d4f4bce9d563a95c65e
manelcharouni <manelcharouni@think-it.io>
2019-08-08 13:31:24 +0100

    added a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,22 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
-
+import MainNavigation from './components/navigation/MainNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect path="/" to='/auth' exact> </Redirect>
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <React.Fragment >
+        <MainNavigation />
+        <main className='main-content'>
+          <Switch>
+            <Redirect path="/" to='/auth' exact> </Redirect>
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
