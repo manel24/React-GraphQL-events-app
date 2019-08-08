845d73dd4474e4cfdbe11546236d53cce8afdf51
manelcharouni <manelcharouni@think-it.io>
2019-08-08 15:12:50 +0100

    added a login form and styled it

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
 
     render() {
-        return (<h1>Auth page</h1>)
+        return (<form className="auth-form">
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+            </div>
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+                <button type="button">Switch to signup</button>
+            </div>
+        </form>)
     }
 }
 
